0a790270b5d5840de3e8e4785206b65f58b5c50f
amirulabdlatib <amirulal73@gmail.com>
2025-09-08 15:49:59 +0800

    Add first example request

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,11 +1,21 @@
-<script setup></script>
+<script setup>
+    const { login: loginAction } = useSanctumAuth();
+    const form = reactive({
+        email: "admin@example.com",
+        password: "password",
+    });
+
+    const login = async () => {
+        await loginAction(form);
+    };
+</script>
 
 <template>
     <div>
         <h1>Login</h1>
-        <form>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+        <form @submit.prevent="login">
+            <input type="email" placeholder="Email" v-model="form.email" />
+            <input type="password" placeholder="Password" v-model="form.password" />
             <button type="submit">Login</button>
         </form>
     </div>
