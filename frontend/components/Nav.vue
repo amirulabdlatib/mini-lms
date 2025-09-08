813b7c04ca2a20477e0fa7341be54abcf65d5210
amirulabdlatib <amirulal73@gmail.com>
2025-09-08 16:24:03 +0800

    Update navbar styling

--- frontend/components/Nav.vue
+++ frontend/components/Nav.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 <template>
-    <div class="p-6 flex items-center justify-between">
+    <div class="p-6 flex items-center justify-between shadow-md">
         <div class="flex items-center space-x-6">
             <NuxtLink to="/">Home</NuxtLink>
             <NuxtLink to="/dashboard">Dashboard</NuxtLink>
         </div>
-        <div class="flex items-center space-x-60">
+        <div class="flex items-center space-x-6">
             <NuxtLink to="/">Account</NuxtLink>
         </div>
     </div>
