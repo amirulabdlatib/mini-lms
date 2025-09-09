6b0c5e78dc5a4a136171f32daa9833dbf94723ac
amirulabdlatib <amirulal73@gmail.com>
2025-09-09 09:27:08 +0800

    Add shadow to the new navbar

--- frontend/components/Nav.vue
+++ frontend/components/Nav.vue
@@ -2,7 +2,7 @@
     const { user, isAuthenticated } = useSanctumAuth();
 </script>
 <template>
-    <header class="bg-white">
+    <header class="bg-white shadow-lg">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
             <div class="flex items-center gap-x-12">
                 <NuxtLink to="/" class="-m-1.5 p-1.5">
