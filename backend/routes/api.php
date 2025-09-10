96c68157ca78167d73f49b6853fb7e2660cd30dd
amirulabdlatib <amirulal73@gmail.com>
2025-09-10 15:08:37 +0800

    Refactor route on api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,10 @@ use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return UserResource::make($request->user());
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/user', function (Request $request) {
+        return UserResource::make($request->user());
+    });
+
+    
+});
