6106669cca9e4643af38299b78e7394d6c504fd1
amirulabdlatib <amirulal73@gmail.com>
2025-09-10 11:45:14 +0800

    Added model,migration,factory,seeder classes, section,student

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Classes;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,12 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
+        ]);
+
+        $this->call([
+            ClassesSeeder::class,
         ]);
     }
 }
