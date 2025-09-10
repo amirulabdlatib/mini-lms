434b20e0c0c954abf4cc1f161de1c212541d3c5c
amirulabdlatib <amirulal73@gmail.com>
2025-09-10 15:17:04 +0800

    Add resource for classes,student and section

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::paginate(10);
+
+        return StudentResource::collection($students);
+    }
 }
